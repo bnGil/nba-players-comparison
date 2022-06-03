d6c876c265b468fb097b4b3c306e77e1fb66ea37
bnGil <gilbn1994@gmail.com>
2022-06-03 16:41:53 +0300

    add some style to the navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,41 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./header.css";
 
 function Header() {
+  const [left, setLeft] = useState(0);
+
+  const handleMouseEnter = (e) => {
+    setLeft(e.target.getBoundingClientRect().x);
+  };
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/comparison">Comparison</Link>
+    <nav className="navbar">
+      <div className="links-container">
+        <NavLink
+          className="navlink"
+          onMouseEnter={handleMouseEnter}
+          exact
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className="navlink"
+          onMouseEnter={handleMouseEnter}
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink
+          className="navlink"
+          onMouseEnter={handleMouseEnter}
+          to="/comparison"
+        >
+          Comparison
+        </NavLink>
+        <div className="stripe" style={{ left: left }}></div>
+      </div>
     </nav>
   );
 }
