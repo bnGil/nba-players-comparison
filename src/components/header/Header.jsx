f7c5989945be477591a4c5557750751edb7f0766
bnGil <gilbn1994@gmail.com>
2022-06-09 01:12:00 +0300

    updated landing page and added style to it

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,42 +1,54 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+
 import "./header.css";
+import logo from "../../assets/images/logo.png";
+import logo2 from "../../assets/images/logo2.png";
 
 function Header() {
   const [left, setLeft] = useState(0);
+  const { pathname } = useLocation();
 
   const handleMouseEnter = (e) => {
     setLeft(e.target.getBoundingClientRect().x);
   };
 
   return (
-    <nav className="navbar">
-      <div className="links-container">
-        <NavLink
-          className="navlink"
-          onMouseEnter={handleMouseEnter}
-          exact
-          to="/"
-        >
-          Home
+    <div
+      className="navbar-container"
+      style={{ backgroundColor: pathname === "/" ? "#151515" : "#000000" }}
+    >
+      <nav className="navbar">
+        <div className="links-container">
+          <NavLink
+            className="navlink"
+            onMouseEnter={handleMouseEnter}
+            exact
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className="navlink"
+            onMouseEnter={handleMouseEnter}
+            to="/about"
+          >
+            About
+          </NavLink>
+          <NavLink
+            className="navlink"
+            onMouseEnter={handleMouseEnter}
+            to="/comparison"
+          >
+            Comparison
+          </NavLink>
+          <div className="stripe" style={{ left: left }}></div>
+        </div>
+        <NavLink exact to="/">
+          <img className="logo" src={logo2} alt="logo" />
         </NavLink>
-        <NavLink
-          className="navlink"
-          onMouseEnter={handleMouseEnter}
-          to="/about"
-        >
-          About
-        </NavLink>
-        <NavLink
-          className="navlink"
-          onMouseEnter={handleMouseEnter}
-          to="/comparison"
-        >
-          Comparison
-        </NavLink>
-        <div className="stripe" style={{ left: left }}></div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
 
--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
 import "./homepage.css";
 
 function Homepage() {
-  return <div>Home page</div>;
+  return (
+    <header className="landing">
+      <div className="landing-content">
+        <h1>COMPARE NBA PLAYERS AND READ WHAT PEOPLE THINK ABOUT THEM</h1>
+        <NavLink to="/about">
+          <button className="btn landing-btn">Learn more</button>
+        </NavLink>
+      </div>
+    </header>
+  );
 }
 
 export default Homepage;
